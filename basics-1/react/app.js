7a40743670b83d9cb72ddd76fafb84c98e3e79a2
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2025-10-09 00:48:44 +0530

    html rendering with js, html rendering using react, with nesting

---
+++ basics-1/react/app.js
@@ -0,0 +1,26 @@
+
+const heading = 
+
+React.createElement("div",{id:"parent"}, // parent
+
+    [
+        React.createElement("div",{id:"child1"}, // child
+
+            [
+                React.createElement("h1",{id:"heading"},"Hello React 👋"), 
+                React.createElement("h2",{id:"sub-heading"},"understandig little by little...")
+            ]
+    ),
+        React.createElement("div", {id:"child2"},
+            [
+                React.createElement("H1", {id:"heading"},"Bye react 👋"),
+                React.createElement("h3",{id:"sub-heading"}, "Getting used to it...step by step...")
+            ]
+    )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
